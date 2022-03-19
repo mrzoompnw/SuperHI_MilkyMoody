fb4deb54d8eb3382085ddb9d7fd00702752266c8
Mr. Zoom <45218774+mrzoompnw@users.noreply.github.com>
2022-03-19 12:08:19 -0700

    Initial commit

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Footer, H2, H2Middle} from './styles'
+
+const FooterComponent = () => (
+  <Footer>
+    <H2>Feb</H2>
+    <H2Middle>World Tour</H2Middle>
+    <H2>Aug</H2>
+  </Footer>
+)
+
+export default FooterComponent
